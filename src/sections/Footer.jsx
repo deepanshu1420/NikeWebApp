fdc79f94d7d641a9cf89320f371f5ea69aa9b94f
Deepanshu Sharma <deepanshusharma1420@gmail.com>
2026-07-14 17:51:58 +0530

    Added animated underline hover effect to navbar links!

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
               <ul>
                 {section.links.map((link) => (
                   <li
-                    className='mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray'
+                    className='mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-coral-red transition-colors duration-300'
                     key={link.name}
                   >
                     <a href={link.link}>{link.name}</a>
