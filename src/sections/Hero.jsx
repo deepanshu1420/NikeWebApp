c6bc7b94d53fa4727b61b572abd25bd438a121b4
Deepanshu Sharma <deepanshusharma1420@gmail.com>
2026-07-15 16:56:22 +0530

    Added animated underline hover effect to navbar links!

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>
-          Our Summer collections!
+          Our Summer Collections!
         </p>
 
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
@@ -51,38 +51,35 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
-      <AnimatePresence mode="sync">
-          <motion.img
+    <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+      <AnimatePresence mode="wait">
+        <motion.img
           key={bigShoeImg}
           src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={502}
           className="object-contain relative z-10"
-          initial={{ opacity: 0.2, scale: 0.97 }}
+          initial={{ opacity: 0, scale: 0.98 }}
           animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0.2, scale: 1.01 }}
-          transition={{
-          duration: 0.14,
-          ease: "easeOut",
-        }}
+          exit={{ opacity: 0, scale: 1.02 }}
+          transition={{ duration: 0.18, ease: "easeOut" }}
         />
       </AnimatePresence>
 
         <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((image, index) => (
-            <div key={index}>
-              <ShoeCard
-                index={index}
-                imgURL={image}
-                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
-                bigShoeImg={bigShoeImg}
-              />
-            </div>
-          ))}
-        </div>
+          <div key={index}>
+            <ShoeCard
+              index={index}
+              imgURL={image}
+              changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+              bigShoeImg={bigShoeImg}
+            />
+          </div>
+        ))}
       </div>
+    </div>
     </section>
   );
 };
