c1db62ddab8e66705a711a0c276161b3f620d7e2
Deepanshu Sharma <deepanshusharma1420@gmail.com>
2026-07-15 12:41:50 +0530

    Added animated underline hover effect to navbar links!

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -67,8 +67,8 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center max-sm:gap-4'>
-        <div className='max-sm:flex max-sm:flex-col max-sm:items-center max-sm:gap-4'>
+      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center max-sm:gap-2'>
+        <div className='max-sm:flex max-sm:flex-col max-sm:items-center max-sm:gap-2'>
         <div className='flex flex-1 justify-start items-center gap-2 font-montserrat'>
           <img
             src={copyrightSign}
